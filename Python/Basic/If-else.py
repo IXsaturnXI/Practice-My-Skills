35170072736f2654d99315384760c4414166a5bb
IXsaturnXI <bdkp123@gmail.com>
2026-08-31 23:25:42 +0700

    another basis
    add another basic python code.
    operation
    if-else

---
+++ Python/Basic/If-else.py
@@ -0,0 +1,7 @@
+i = int(input("Enter a number: "))
+if i > 0 and i < 5:
+    print("The number is not enough")
+elif i >= 5 and i < 10:
+    print("The number is enough")
+else:
+    print("The number is too much")
